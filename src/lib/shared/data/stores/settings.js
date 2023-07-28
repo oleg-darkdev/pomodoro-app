fca8ff910386e0995872a7232a7611022d663749
DarkDev <darkdev_brand@riseup.net>
2023-07-28 11:02:45 +0300

    refactoring

--- src/lib/shared/data/stores/settings.js
+++ src/lib/shared/data/stores/settings.js
@@ -1,16 +1,11 @@
 import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
 
-let theme = writable('#CB2A37'),
-	currentView = writable('pomo'),
+let
 	hideHour = writable(false);
 
 if (browser) {
-	theme = writable(localStorage.getItem('theme'));
-
-	currentView = writable(localStorage.getItem('view'));
-
 	hideHour = writable(JSON.parse(localStorage.getItem('hideHour')) || false);
 }
 
-export { theme, currentView, hideHour };
+export {  hideHour };
