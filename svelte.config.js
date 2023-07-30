e15a7a7f1281579d1a6a398d3da444036ae0cea7
DarkDev <darkdev_brand@riseup.net>
2023-07-30 20:47:31 +0300

    add aliesies

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
+import path from 'path';
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-
-  kit: {
-    adapter: adapter()
-  }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			'@coreEntities': path.resolve('./src/lib/core/layout/entities'),
+			'@coreFeatures': path.resolve('./src/lib/core/layout/features'),
+			'@coreProcesses': path.resolve('./src/lib/core/layout/processes'),
+			'@coreShared': path.resolve('./src/lib/core/layout/shared'),
+			'@coreWidgets': path.resolve('./src/lib/core/layout/widgets'),
+		}
+	}
 };
 
 export default config;
