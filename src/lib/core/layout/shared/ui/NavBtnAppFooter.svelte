199d5f15df195345d00958b751695833afa79bde
DarkDev <darkdev_brand@riseup.net>
2023-07-30 20:50:24 +0300

    intial like classic android app routing

---
+++ src/lib/core/layout/shared/ui/NavBtnAppFooter.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let text, activeScreen;
+</script>
+
+<button  on:click class={text === activeScreen? 'active bg-pink-600': 'bg-black'}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="h-5 w-5"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+	>
+		<slot />
+	</svg>
+	<span class="btn-nav-label">{text}</span>
+</button>
